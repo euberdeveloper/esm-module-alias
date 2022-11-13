22f6e77085a00ccff212c1e3ed21a98753d198f4
euberdeveloper <euberdeveloper@gmail.com>
2022-11-13 19:16:04 +0100

    test: add tests with jest and shelljs

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -1,5 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const shell = require('shelljs');
+
 describe('Integration tests', function () {
-    it('Simple test', function () {
-        expect(1).toEqual(1)
-    });
+    const sampleProjectsPath = path.join(__dirname, 'sample-projects');
+    const sampleProjectsRoots = fs.readdirSync(sampleProjectsPath).map(dirName => path.join(sampleProjectsPath, dirName));
+
+    const customErrorMessageTemplate = fs.readFileSync(path.join(__dirname, 'assets', 'custom-error-message.template.txt'), 'utf-8');
+
+    for (const sampleProjectPath of sampleProjectsRoots) {
+        const baseName = path.basename(sampleProjectPath);
+        it(`Test sample project ${baseName}`, function () {
+            const { code, stdout, stderr } = shell.exec('npm test', {
+                cwd: sampleProjectPath
+            });
+
+            const errorMessage = customErrorMessageTemplate
+                .replaceAll('{{CODE}}', code)
+                .replaceAll('{{STDOUT}}', stdout)
+                .replaceAll('{{STDERR}}', stderr);
+
+            expect(code, errorMessage).toEqual(0);
+        });
+    }
 });
