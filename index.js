588f44c2ac32db1db7292cf72cf0464cfdfe6134
euberdeveloper <euberdeveloper@gmail.com>
2023-10-31 11:40:36 +0100

    feat(index.js): add possibility for a custom matcher
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import path from 'node:path';
 
-export default function generateAliasesResolver(aliasesToAdd) {
+export default function generateAliasesResolver(aliasesToAdd, customMatcher) {
   const getAliases = () => {
 
     const base = process.cwd();
@@ -16,10 +16,10 @@ export default function generateAliasesResolver(aliasesToAdd) {
 
   }
 
-  const isAliasInSpecifier = (path, alias) => {
+  const isAliasInSpecifier = customMatcher ?? ((path, alias) => {
     return path.indexOf(alias) === 0
       && (path.length === alias.length || path[alias.length] === '/');
-  };
+  });
 
   const aliases = getAliases();
 
