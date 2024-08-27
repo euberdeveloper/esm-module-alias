aa8de47836c8237eadc6e37c9767fa9277142893
euberdeveloper <euberdeveloper@gmail.com>
2024-08-27 23:51:27 +0200

    test: add test for relative path outside of project
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -4,15 +4,18 @@ const shell = require('shelljs');
 
 describe('Integration tests', function () {
     const sampleProjectsPath = path.join(__dirname, 'sample-projects');
-    const sampleProjectsRoots = fs.readdirSync(sampleProjectsPath).map(dirName => path.join(sampleProjectsPath, dirName));
-
+    const sampleProjectsRoots = fs.readdirSync(sampleProjectsPath)
+        .map(dirName => path.join(sampleProjectsPath, dirName));
     const customErrorMessageTemplate = fs.readFileSync(path.join(__dirname, 'assets', 'custom-error-message.template.txt'), 'utf-8');
 
     for (const sampleProjectPath of sampleProjectsRoots) {
         const baseName = path.basename(sampleProjectPath);
         it(`Test sample project ${baseName}`, function () {
+            const projectPath = baseName === 'relative-path-outside-project' 
+                ? path.join(sampleProjectPath, 'project') 
+                : sampleProjectPath;
             const { code, stdout, stderr } = shell.exec('npm test', {
-                cwd: sampleProjectPath
+                cwd: projectPath
             });
 
             const errorMessage = customErrorMessageTemplate
